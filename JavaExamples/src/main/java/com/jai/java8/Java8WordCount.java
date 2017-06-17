871281dff2f1c68fa1ecb322354b7e814f31d1ed
jaigates <jaigates@gmail.com>
2017-06-17 19:25:31 -0400

    updates

--- JavaExamples/src/main/java/com/jai/java8/Java8WordCount.java
+++ JavaExamples/src/main/java/com/jai/java8/Java8WordCount.java
@@ -1,6 +1,5 @@
 package com.jai.java8;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,32 +15,37 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 
 public class Java8WordCount {
-	
+
 	public static Logger log = LoggerFactory.getLogger(Java8WordCount.class);
 
 	public static void main(String[] args) {
-		
-		//String fn =("./big.txt");
-		//String fn = "./500krows.csv";
+
+		// String fn =("./big.txt");
+		// String fn = "./500krows.csv";
 		String fn = "majestic_million.csv";
+		wordcount1(fn);
+	}
+
+	public static void wordcount1(String fn) {
+
 		StopWatch sw = new StopWatch(fn);
 		sw.start("wordcount");
 		Path path = Paths.get(fn);
 		String DELIMITER = ",";
-		log.info(path.toAbsolutePath()+"-"+path.toFile().exists());
-		
+		log.info(path.toAbsolutePath() + "-" + path.toFile().exists());
+
 		try {
-			Stream<String> lines = Files.lines(path,Charset.forName(CharsetUtil.getCharset(fn)));
-			//count(lines);
-			Stream<String> flatMap = lines.flatMap(line ->
-				{
-					String[] split = line.trim().split(DELIMITER);
-					return Arrays.stream(split);
-				});
-			//count(flatMap);
-			//count(filter);
-			Map<String, Integer> wordCount = flatMap.map(word -> word.toLowerCase().trim()).filter(word -> word.length() > 0)
-					.map(word -> new SimpleEntry<>(word, 1)).sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+			Stream<String> lines = Files.lines(path, Charset.forName(CharsetUtil.getCharset(fn)));
+			// count(lines);
+			Stream<String> flatMap = lines.flatMap(line -> {
+				String[] split = line.trim().split(DELIMITER);
+				return Arrays.stream(split);
+			});
+			// count(flatMap);
+			// count(filter);
+			Map<String, Integer> wordCount = flatMap.map(word -> word.toLowerCase().trim())
+					.filter(word -> word.length() > 0).map(word -> new SimpleEntry<>(word, 1))
+					.sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
 					.reduce(new LinkedHashMap<>(), (acc, entry) -> {
 						acc.put(entry.getKey(), acc.compute(entry.getKey(), (k, v) -> v == null ? 1 : v + 1));
 						return acc;
@@ -53,21 +57,23 @@ public class Java8WordCount {
 			wordCount.forEach((k, v) -> log.info(String.format("%s ----- %d", k, v)));
 		} catch (Exception e) {
 			e.printStackTrace();
-			log.error("",e);
+			log.error("", e);
 		}
-		
+
 		sw.stop();
 		log.info(sw.prettyPrint());
-		
-		
-		int fordebug = 0;
 
+		int fordebug = 0;
+	}
+
+	public static Map<String, Long> countWords(Stream<String> names) {
+		return names.collect(groupingBy(name -> name, counting()));
 	}
 
 	private static void count(Stream<?> filter) {
-		
+
 		long count = filter.count();
-		log.info("Stream count : "+count);
-		
+		log.info("Stream count : " + count);
+
 	}
 }
