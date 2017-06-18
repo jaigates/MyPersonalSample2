97bb03809189f7f53e5d3c6d8367a5ebe37492d0
jaigates <jaigates@gmail.com>
2017-06-18 16:44:13 -0400

    to run in eclipse

--- JavaExamples/src/main/scala/com/jai/spark/streaming/StreamingExample1.scala
+++ JavaExamples/src/main/scala/com/jai/spark/streaming/StreamingExample1.scala
@@ -15,27 +15,29 @@ import org.apache.spark.streaming.{ Seconds, StreamingContext }
  * Then create a text file in `localdir` and the words in the file will get counted.
  */
 object StreamingExample1 {
-  
+
   def main(args: Array[String]) {
-    if (args.length < 1) {
-      System.err.println("Usage: HdfsWordCount <directory>")
-      System.exit(1)
-    }
+
+    val fn = if (args.length < 1)
+      "./data/big.txt"
+    else
+      args(0)
 
     val sparkConf = new SparkConf().setAppName("HdfsWordCount")
+    sparkConf.setMaster("local")
     // Create the context
     val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     // Create the FileInputDStream on the directory and use the
     // stream to count words in new files created
-    val lines = ssc.textFileStream(args(0))
+    val lines = ssc.textFileStream(fn)
     val words = lines.flatMap(_.split(" "))
     val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
     wordCounts.print()
     ssc.start()
     ssc.awaitTermination()
   }
-  
+
 }
 	// scalastyle:on println
 
